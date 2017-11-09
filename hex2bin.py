64cae989c4b210040e237d6cedae70f373d5f6b1
David Poole <testcluster@gmail.com>
2017-11-09 06:39:39 -0700

    new work: send/recv files netcat style; hexdump 2 bin
    hex2bin a work in progress

---
+++ hex2bin.py
@@ -0,0 +1,35 @@
+# davep 20171107 ; convert file of space separated printed hex bytes into a raw
+# byte file
+
+import sys
+import itertools
+
+# https://docs.python.org/3/library/itertools.html#itertools-recipes
+def grouper(iterable, n, fillvalue=None):
+	"Collect data into fixed-length chunks or blocks"
+	# grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+	args = [iter(iterable)] * n
+	return itertools.zip_longest(*args, fillvalue=fillvalue)
+
+def main():
+	infilename = sys.argv[1]
+	outfilename = sys.argv[2]
+
+	with open(infilename,"r") as infile:
+		hexdump = " ".join([s.strip() for s in infile.readlines()])
+
+	# block of hex as 2 nibbles separated by spaces
+	# .e.g, 18 60 30 14 01 00 ...
+#	buf = bytes([int(c,16) for c in hexdump.split()])
+
+	# solid block of text (no spaces)
+	# e.g., 64886F70D010...
+#	for c in grouper(hexdump, 2):
+#		print(c)
+	buf = bytes([int(''.join(c),16) for c in grouper(hexdump, 2)])
+
+	with open(outfilename,"wb") as outfile:
+		outfile.write(buf)
+
+if __name__ == '__main__':
+    main()
---
+++ nc_send.py
@@ -0,0 +1,29 @@
+# https://stackoverflow.com/questions/1908878/netcat-implementation-in-python
+import sys
+import socket
+
+def netcat(host, port, content):
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((host, int(port)))
+	s.sendall(content.encode())
+	s.shutdown(socket.SHUT_WR)
+	while True:
+		data = s.recv(4096)
+		if not data:
+			break
+		print(repr(data))
+	s.close()
+
+# davep 20170728 ; my code 
+def main():
+	ip_address = sys.argv[1]
+	port = int(sys.argv[2])
+	infilename = sys.argv[3]
+
+	with open(infilename,"r") as infile:
+		buf = infile.read()
+
+	netcat(ip_address, port, buf)
+
+if __name__=='__main__':
+	main()
