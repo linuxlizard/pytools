b17d9aa8a01df6bc04c938346106cb89e81d032c
David Poole <testcluster@gmail.com>
2017-11-09 06:40:16 -0700

    handle unicode failure; dump files from argv

--- hexdump.py
+++ hexdump.py
@@ -5,33 +5,28 @@
 FILTER=''.join([(len(repr(chr(x)))==3) and chr(x) or '.' for x in range(256)])
 
 def dump(src, length=8):
-    N=0; result=''
-    try:
-        src = src.decode("ascii")
-    except AttributeError:
-        pass
-    while src:
-       s,src = src[:length],src[length:]
-       hexa = ' '.join(["%02X"%ord(x) for x in s])
-       s = s.translate(FILTER)
-       result += "%04X   %-*s   %s\n" % (N, length*3, hexa, s)
-       N+=length
-    return result
+	N=0; result=''
+	while src:
+		s,src = src[:length],src[length:]
+		hexa = ' '.join(["%02X"%ord(x) for x in s])
+		s = s.translate(FILTER)
+		result += "%04X   %-*s   %s\n" % (N, length*3, hexa, s)
+		N+=length
+	return result
 
 def dump2(src, length=8):
-    result=[]
-    for i in xrange(0, len(src), length):
-       s = src[i:i+length]
-       hexa = ' '.join(["%02X"%ord(x) for x in s])
-       printable = s.translate(FILTER)
-       result.append("%04X   %-*s   %s\n" % (i, length*3, hexa, printable))
-    return ''.join(result)
+	result=[]
+	for i in xrange(0, len(src), length):
+		s = src[i:i+length]
+		hexa = ' '.join(["%02X"%ord(x) for x in s])
+		printable = s.translate(FILTER)
+		result.append("%04X   %-*s   %s\n" % (i, length*3, hexa, printable))
+	return ''.join(result)
 
 if __name__ == '__main__' :
-    s=("This 10 line function is just a sample of python power "
-       "for string manipulations.\n"
-       "The code is \x07even\x08 quite readable!")
-
-#    print dump(s, 16)
-#    print dump2(s, 16 )
+	import sys
+	for f in sys.argv[1:]:
+		with open(f,"rb") as infile:
+			buf = infile.read().decode("ascii")
+		print(dump(buf,16))
 
