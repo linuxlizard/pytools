9be382cef5bdcaebd8890f950444a30727e2eb24
David Poole <dpoole@cradlepoint.com>
2020-12-07 15:01:10 -0700

    open in binary; run through black

--- nc_send.py
+++ nc_send.py
@@ -2,28 +2,32 @@
 import sys
 import socket
 
+
 def netcat(host, port, content):
-	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	s.connect((host, int(port)))
-	s.sendall(content.encode())
-	s.shutdown(socket.SHUT_WR)
-	while True:
-		data = s.recv(4096)
-		if not data:
-			break
-		print(repr(data))
-	s.close()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host, int(port)))
+    s.sendall(content.encode())
+    s.shutdown(socket.SHUT_WR)
+    while True:
+        data = s.recv(4096)
+        if not data:
+            break
+        print(repr(data))
+    s.close()
 
-# davep 20170728 ; my code 
+
+# davep 20170728 ; my code
 def main():
-	ip_address = sys.argv[1]
-	port = int(sys.argv[2])
-	infilename = sys.argv[3]
+    ip_address = sys.argv[1]
+    port = int(sys.argv[2])
+    infilename = sys.argv[3]
 
-	with open(infilename,"r") as infile:
-		buf = infile.read()
+    with open(infilename, "rb") as infile:
+        buf = infile.read()
 
-	netcat(ip_address, port, buf)
+    netcat(ip_address, port, buf)
 
-if __name__=='__main__':
-	main()
+
+if __name__ == "__main__":
+    main()
+
