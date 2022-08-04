d778c09b091035fbcb6ffd5d50efab985b0e69da
David Poole <dpoole@cradlepoint.com>
2022-08-04 09:16:20 -0600

    convert tabs/spaces ; clean up debug messages

--- hex2bin.py
+++ hex2bin.py
@@ -6,30 +6,33 @@ import itertools
 
 # https://docs.python.org/3/library/itertools.html#itertools-recipes
 def grouper(iterable, n, fillvalue=None):
-	"Collect data into fixed-length chunks or blocks"
-	# grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
-	args = [iter(iterable)] * n
-	return itertools.zip_longest(*args, fillvalue=fillvalue)
+    "Collect data into fixed-length chunks or blocks"
+    # grouper('ABCDEFG', 3, 'x') --> ABC DEF Gxx"
+    args = [iter(iterable)] * n
+    return itertools.zip_longest(*args, fillvalue=fillvalue)
 
 def main():
-	infilename = sys.argv[1]
-	outfilename = sys.argv[2]
+    infilename = sys.argv[1]
+    outfilename = sys.argv[2]
 
-	with open(infilename,"r") as infile:
-		hexdump = " ".join([s.strip() for s in infile.readlines()])
+    with open(infilename,"r") as infile:
+#        for line in infile.readlines():
+#            print(line)
+#            print(line[7:56].strip())
+        hexdump = " ".join([s[7:56].strip() for s in infile.readlines()])
 
-	# block of hex as 2 nibbles separated by spaces
-	# .e.g, 18 60 30 14 01 00 ...
-#	buf = bytes([int(c,16) for c in hexdump.split()])
+    # block of hex as 2 nibbles separated by spaces
+    # .e.g, 18 60 30 14 01 00 ...
+    buf = bytes([int(c,16) for c in hexdump.split()])
 
-	# solid block of text (no spaces)
-	# e.g., 64886F70D010...
-#	for c in grouper(hexdump, 2):
-#		print(c)
-	buf = bytes([int(''.join(c),16) for c in grouper(hexdump, 2)])
+    # solid block of text (no spaces)
+    # e.g., 64886F70D010...
+#    for c in grouper(hexdump, 2):
+#        print(c)
+#    buf = bytes([int(''.join(c),16) for c in grouper(hexdump, 2)])
 
-	with open(outfilename,"wb") as outfile:
-		outfile.write(buf)
+    with open(outfilename,"wb") as outfile:
+        outfile.write(buf)
 
 if __name__ == '__main__':
     main()
