3e58c99e571b013075eac0eec4fe09aaf3e8c554
David Poole <testcluster@gmail.com>
2024-04-01 11:31:30 -0600

    convert to 100% passive mode STOR

--- ftpdpy/ftpstor.py
+++ ftpdpy/ftpstor.py
@@ -2,9 +2,11 @@
 
 # Command line one shot ftp STOR a file to FTP server.
 # Written to send files from emebedded linux to simple FTP server.
+# Very little error handling/recovery. Quick and dirty code.
 #
 # davep 20240401
 
+import re
 import os
 import sys
 import socket
@@ -22,42 +24,43 @@ BUFSIZE = 1024
 USERNAME = "anonymous"
 PASSWORD = "nobody@example.com"
 
-def parse_ip(s):
-    fields = s.split(".")
-    assert len(fields)==4, len(fields)
-    return [int(f) for f in fields]
+# lifted this code from ftplib.py's parse227()
+_port_re = re.compile(r'(\d+),(\d+),(\d+),(\d+),(\d+),(\d+)')
 
 def txrx(sock, msg):
     # dumb send a message, recv a response
     sock.sendall(msg.encode("utf8"))
     buf = sock.recv(BUFSIZE)
-    print(f"buf={buf}")
+    print(f"recv buf={buf}")
+    return buf.decode("utf8")
 
-def ftp_store(my_ip, server_ip, filename):
+def parse_passive_response(s):
+    # lifted this code from ftplib.py's parse227()
+    # note no error checking
+    m = _port_re.search(s)
+    numbers = m.groups()
+    ip = '.'.join(numbers[:4])
+    port = (int(numbers[4]) << 8) + int(numbers[5])
+    return ip, port
+    
+def ftp_store_passive(server_ip, filename):
     ctrl_sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
-    data_sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
-    data_sock.bind(('',0))
-    data_sock.listen(1)
-    data_port = data_sock.getsockname()[1]
-    print(f"port={data_port}")
-
     ctrl_sock.connect( (server_ip, FTP_PORT) )
 
     # recv the header
     buf = ctrl_sock.recv(BUFSIZE)
-    print(f"buf={buf}")
+    print(f"recv buf={buf}")
 
     txrx(ctrl_sock, "USER %s\r\n" % USERNAME)
     txrx(ctrl_sock, "PASS %s\r\n" % PASSWORD)
     txrx(ctrl_sock, "TYPE I\r\n")
 
-    port_arg = ( *parse_ip(my_ip), (data_port >> 8), data_port & 0xff)
-    txrx(ctrl_sock, "PORT %d,%d,%d,%d,%d,%d\r\n" % port_arg)
+    buf = txrx(ctrl_sock, "PASV\r\n")
+    ip, port = parse_passive_response(buf)
+    data_sock = socket.socket( socket.AF_INET, socket.SOCK_STREAM )
+    data_sock.connect( (ip, port) )
 
     txrx(ctrl_sock, "STOR %s\r\n" % os.path.split(filename)[1])
-
-    (request_sock,(client_address,client_port)) = data_sock.accept()
-    print(f"request={request_sock} client={client_address} port={client_port}")
 
     print(f"sending file...")
     with open(filename, "rb") as infile:
@@ -66,21 +69,18 @@ def ftp_store(my_ip, server_ip, filename):
             print(f"read bytes={len(buf)}")
             if len(buf) == 0:
                 break
-            request_sock.sendall(buf)
+            data_sock.sendall(buf)
 
-    request_sock.close()
-#    data_sock.close()
-
+    data_sock.close()
     txrx(ctrl_sock, "QUIT\r\n")
 
 if len(sys.argv) < 4:
     print("usage: %s my-ipaddress server-ipaddress file0 [file1 [file2...]]" % sys.argv[0])
     sys.exit(1)
 
-my_ip = sys.argv[1]
-server_ip = sys.argv[2]
-filename_list = sys.argv[3:]
+server_ip = sys.argv[1]
+filename_list = sys.argv[2:]
 
 for f in filename_list:
-    ftp_store(my_ip, server_ip, f)
+    ftp_store_passive(server_ip, f)
 
